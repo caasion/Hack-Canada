b8e962b4f37f4e8c51162ee1837536cc83f9634d
Samuel <124836035+samzeh@users.noreply.github.com>
2026-03-07 13:19:21 -0500

    cloudinary image smart crop

---
+++ cloudinary/cloudinary.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+async function uploadImage(imageUrl) {
+  const result = await cloudinary.uploader.unsigned_upload(imageUrl, process.env.CLOUDINARY_UPLOAD_PRESET);
+  return {
+    public_id: result.public_id,
+    secure_url: result.secure_url,
+    width: result.width,
+    height: result.height,
+  };
+}
+
+function generateSmartCroppedUrl(publicId, options = {}) {
+  return cloudinary.url(publicId, {
+    crop: "thumb",
+    gravity: "center",
+    width: options.width || 300,
+    height: options.height || 300,
+    quality: "100",
+    sign_url: true
+  });
+}
+
+module.exports = { uploadImage, generateSmartCroppedUrl };
--- serpapi/serpapi.server.js
+++ serpapi/serpapi.server.js
@@ -3,7 +3,7 @@ const { getJson } = require("serpapi");
 
 getJson({
   engine: "google_lens",
-  url: "https://ecotonelamisport.ca/wp-content/uploads/2021/08/YETI-RAMBLER-BOUTEILLE-DEAU-ISOTHERME-DE-1L-36-OZ-BSB.jpg",
+  url: "https://res.cloudinary.com/dqxybdhql/image/upload/c_thumb,e_sharpen:200,g_center,h_300,q_100,w_300,z_3.0/j6pr0flvnf0qzcjh7ior?_a=BAMAOGLU0",
   api_key: process.env.SERP_API_KEY
 }, (json) => {
   const withPrice = json["visual_matches"].filter(item => item.price);
